7fed4e9f0bd422a9b1439093909cc6b79d475168
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2023-01-07 12:26:58 +0100

    task update is done in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -7,12 +7,12 @@ import Header from "./Header";
 import Container from "./Container";
 
 function App() {
-
   const [hideDone, setHideDone] = useState(false);
-
   const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
-
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const hideDoneTasks = () => {
     setHideDone(hideDone => !hideDone);
@@ -37,7 +37,7 @@ function App() {
 
   const timeTaskAdded = () => {
     const date = new Date();
-    const time = date.toLocaleTimeString(undefined, {hour: "2-digit", minute: "2-digit"});
+    const time = date.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" });
     return `${time}`;
   };
 
